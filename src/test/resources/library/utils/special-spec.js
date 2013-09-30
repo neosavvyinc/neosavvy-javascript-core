dc196968d98ea02a4956f2df385c58b41d8e2d39
Trevor Ewen <tewen@neosavvy.com>
2013-09-30 13:21:37 -0400

    Test coverage for the special utils.

---
+++ src/test/resources/library/utils/special-spec.js
@@ -0,0 +1,60 @@
+describe("Neosavvy.Core.Utils.SpecialUtils", function () {
+
+    describe("keepTrying", function () {
+        it("Should return null when called with no arguments", function () {
+            expect(Neosavvy.Core.Utils.SpecialUtils.keepTrying()).toBeNull();
+        });
+
+        it("Should throw an error when called with an odd number of arguments", function () {
+            expect(function () {
+                Neosavvy.Core.Utils.SpecialUtils.keepTrying(function () {
+                }, [], function () {
+                });
+            }).toThrow();
+        });
+
+        it("Should return the value of the first function in the list if it does not throw an error", function () {
+            expect(Neosavvy.Core.Utils.SpecialUtils.keepTrying(
+                function () {
+                    return 6;
+                }, [], function () {
+                    throw "Some error!";
+                }, [])
+            ).toEqual(6);
+        });
+
+        it("Should return the value of the second function in the list if the first throws an error", function () {
+            expect(Neosavvy.Core.Utils.SpecialUtils.keepTrying(
+                function () {
+                    throw "Some error!";
+                }, [88], function () {
+                    return arguments[0];
+                }, [77])
+            ).toEqual(77);
+        });
+
+        it("Should return the value of n function in the list if 0 >> n - 1 throw errors", function () {
+            expect(Neosavvy.Core.Utils.SpecialUtils.keepTrying(
+                function () {
+                    throw "Some error!";
+                }, [88],
+                function () {
+                    throw "Some error!";
+                }, [98],
+                function () {
+                    throw "Some error!";
+                }, [108],
+                function () {
+                    throw "Some error!";
+                }, [188],
+                function () {
+                    throw "Some error!";
+                }, [1188],
+                function () {
+                    return arguments[0];
+                }, [42])
+            ).toEqual(42);
+        });
+    });
+
+});
