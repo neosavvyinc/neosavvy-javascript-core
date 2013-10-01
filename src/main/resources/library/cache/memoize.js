9e11970c725fed7ee452bb58b47f740b282b401b
UnicodeSnowman <utfsnowman@gmail.com>
2013-10-01 10:52:57 -0400

    fixing docs for memoize

--- src/main/resources/library/cache/memoize.js
+++ src/main/resources/library/cache/memoize.js
@@ -1,12 +1,16 @@
 /**
- * @class global
- * Memoize caches input output functions return values. Just alter the function by passing it into memoize, and you have the cached version.
- * @param {Function} func
- * @returns Function
- * @method memoize
+ * @class window
+ * @static
  **/
 (function (global) {
     "use strict";
+    /**
+     * Memoize caches input output functions return values. Just alter the function by passing it into memoize, and you have the cached version.
+     * @method memoize
+     * @param {Function} func
+     * @returns Function
+     *
+     **/
     global.memoize || (global.memoize = (typeof JSON === 'object' && typeof JSON.stringify === 'function' ?
         function (func) {
             var stringifyJson = JSON.stringify,
