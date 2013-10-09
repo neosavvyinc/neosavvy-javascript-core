5a7c8d6ebabf7e35f2bd4bea42d76af0fc124c52
Trevor Ewen <tewen@neosavvy.com>
2013-10-09 16:15:48 -0400

    Updated the map utils to include a high performance get function.

--- src/main/resources/library/utils/map.js
+++ src/main/resources/library/utils/map.js
@@ -14,14 +14,14 @@ Neosavvy.Core.Utils.MapUtils = (function () {
          * @param {Obj} map
          * @param {String} properties
          * @returns Obj
-         * @method itemByProperty
+         * @method get
          *
          * @example
-            get({name: 'Bob Pollard'}, 'name') => 'Bob Pollard'
-            get({location: {state: 'OH', city: 'Dayton'}}, 'location') => { state: 'OH', city: 'Dayton' }
-            get({location: {state: 'OH', city: 'Dayton'}}, 'location.city') => 'Dayton'
+         get({name: 'Bob Pollard'}, 'name') => 'Bob Pollard'
+         get({location: {state: 'OH', city: 'Dayton'}}, 'location') => { state: 'OH', city: 'Dayton' }
+         get({location: {state: 'OH', city: 'Dayton'}}, 'location.city') => 'Dayton'
          **/
-        get:function (map, properties) {
+        get: function (map, properties) {
             if (map && properties) {
                 properties = properties.split(".");
                 while (properties.length) {
@@ -35,17 +35,67 @@ Neosavvy.Core.Utils.MapUtils = (function () {
             return map;
         },
         /**
+         * returns the value in map that matches the passed in property.
+         * also supports dotted properties. Limited to 10 length property chain.
+         * @param {Obj} map
+         * @param {String} properties
+         * @returns Obj
+         * @method highPerformanceGet
+         *
+         * @example
+         highPerformanceGet({name: 'Bob Pollard'}, 'name') => 'Bob Pollard'
+         highPerformanceGet({location: {state: 'OH', city: 'Dayton'}}, 'location') => { state: 'OH', city: 'Dayton' }
+         highPerformanceGet({location: {state: 'OH', city: 'Dayton'}}, 'location.city') => 'Dayton'
+         **/
+        highPerformanceGet: function (map, properties) {
+            if (map && properties) {
+                properties = properties.split(".");
+                if (properties.length > 10) {
+                    throw "You cannot pass in a string of properties greater than 10";
+                }
+                try {
+                    switch (properties.length) {
+                        case 0:
+                            return map
+                        case 1:
+                            return map[properties[0]];
+                        case 2:
+                            return map[properties[0]][properties[1]];
+                        case 3:
+                            return map[properties[0]][properties[1]][properties[2]];
+                        case 4:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]];
+                        case 5:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]][properties[4]];
+                        case 6:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]][properties[4]][properties[5]];
+                        case 7:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]][properties[4]][properties[5]][properties[6]];
+                        case 8:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]][properties[4]][properties[5]][properties[6]][properties[7]];
+                        case 9:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]][properties[4]][properties[5]][properties[6]][properties[7]][properties[8]];
+                        case 10:
+                            return map[properties[0]][properties[1]][properties[2]][properties[3]][properties[4]][properties[5]][properties[6]][properties[7]][properties[8]][properties[9]];
+                    }
+                } catch (e) {
+                    return undefined;
+                }
+            }
+            return map;
+        },
+        /**
          * returns true or false based on whether or not the passed
          * in set of objects all have unique keys
          *
          * @example
-            keysDistinct({whoomp: 'there it is'}, {whoomp: 'here it goes'}) => false
-            keysDistinct({whoomp: 'there it is'}, {tagTeam: 'back again'}) => true
+         keysDistinct({whoomp: 'there it is'}, {whoomp: 'here it goes'}) => false
+         keysDistinct({whoomp: 'there it is'}, {tagTeam: 'back again'}) => true
          * @param {obj} arguments
          * @returns Boolean
          * @method keysDistinct
          **/
-        keysDistinct:function() {
+        keysDistinct: function () {
             if (arguments.length > 1) {
                 var accumulatedLength = 0;
                 for (var i = 0; i < arguments.length; i++) {
--- src/test/resources/library/utils/map-spec.js
+++ src/test/resources/library/utils/map-spec.js
@@ -1,27 +1,55 @@
 describe("Neosavvy.Core.Utils.MapUtils", function () {
     describe("get", function () {
-        describe("get", function () {
-            it("Should return null or undefined respectively if the map is so", function () {
-                expect(Neosavvy.Core.Utils.MapUtils.get(undefined, "name")).toBeUndefined();
-                expect(Neosavvy.Core.Utils.MapUtils.get(null, "name")).toBeNull();
-            });
+        it("Should return null or undefined respectively if the map is so", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.get(undefined, "name")).toBeUndefined();
+            expect(Neosavvy.Core.Utils.MapUtils.get(null, "name")).toBeNull();
+        });
 
-            it("Should return the map itself if the properties are not provided", function () {
-                expect(Neosavvy.Core.Utils.MapUtils.get({name: "Charlie"}, null)).toEqual({name: "Charlie"});
-            });
+        it("Should return the map itself if the properties are not provided", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.get({name: "Charlie"}, null)).toEqual({name: "Charlie"});
+        });
 
-            it("Should play nice with single properties", function () {
-                expect(Neosavvy.Core.Utils.MapUtils.get({name: "Charlie"}, "name")).toEqual("Charlie");
-                expect(Neosavvy.Core.Utils.MapUtils.get({name: "Charlie", number: 7}, "number")).toEqual(7);
-            });
+        it("Should play nice with single properties", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.get({name: "Charlie"}, "name")).toEqual("Charlie");
+            expect(Neosavvy.Core.Utils.MapUtils.get({name: "Charlie", number: 7}, "number")).toEqual(7);
+        });
 
-            it("Should play nice with dot properties", function () {
-                expect(Neosavvy.Core.Utils.MapUtils.get({name: {first: "Charlie", town: "White Plains"}}, "name.town")).toEqual("White Plains");
-            });
+        it("Should play nice with dot properties", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.get({name: {first: "Charlie", town: "White Plains"}}, "name.town")).toEqual("White Plains");
+        });
 
-            it("Should play nice with properties that don't exist, by returning undefined", function () {
-                expect(Neosavvy.Core.Utils.MapUtils.get({name: {first: "Charlie", town: "White Plains"}}, "name.town.country.something")).toBeUndefined();
-            });
+        it("Should play nice with properties that don't exist, by returning undefined", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.get({name: {first: "Charlie", town: "White Plains"}}, "name.town.country.something")).toBeUndefined();
+        });
+    });
+
+    describe("highPerformanceGet", function () {
+        it("Should return null or undefined respectively if the map is so", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet(undefined, "name")).toBeUndefined();
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet(null, "name")).toBeNull();
+        });
+
+        it("Should return the map itself if the properties are not provided", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet({name: "Charlie"}, null)).toEqual({name: "Charlie"});
+        });
+
+        it("Should play nice with single properties", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet({name: "Charlie"}, "name")).toEqual("Charlie");
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet({name: "Charlie", number: 7}, "number")).toEqual(7);
+        });
+
+        it("Should play nice with dot properties", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet({name: {first: "Charlie", town: "White Plains"}}, "name.town")).toEqual("White Plains");
+        });
+
+        it("Should play nice with properties that don't exist, by returning undefined", function () {
+            expect(Neosavvy.Core.Utils.MapUtils.highPerformanceGet({name: {first: "Charlie", town: "White Plains"}}, "name.town.country.something")).toBeUndefined();
+        });
+
+        it("Should throw an error when the property string is greater than 10 items long", function () {
+            expect(function () {
+                Neosavvy.Core.Utils.MapUtils.highPerformanceGet({}, "1.2.3.4.5.6.7.8.9.10.11")
+            }).toThrow();
         });
     });
 
