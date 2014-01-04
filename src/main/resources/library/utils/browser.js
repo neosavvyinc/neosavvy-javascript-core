f3ae77f7c685a37f9e6c0e032f7901fc2f55c304
Trevor Ewen <tewen@neosavvy.com>
2014-01-04 17:22:01 -0500

    Completed tests and functionality for browser utils.

--- src/main/resources/library/utils/browser.js
+++ src/main/resources/library/utils/browser.js
@@ -31,7 +31,28 @@ Neosavvy.Core.Utils.BrowserUtils = (function () {
         //OS
         var androidOs = /(android)/.exec(userAgent);
         os = (androidOs && androidOs.length) ? 'android' : /(mac|win|linux|freebsd|mobile|iphone|ipod|ipad|android|blackberry|j2me|webtv)/.exec(userAgent)[1];
-        osVersion = null;
+
+        //OS Version
+        switch (os) {
+            case _CONSTANTS.OS.OSX:
+            case _CONSTANTS.OS.IPHONE:
+            case _CONSTANTS.OS.IPAD:
+            case _CONSTANTS.OS.WINDOWS:
+            case _CONSTANTS.OS.ANDROID:
+            case _CONSTANTS.OS.BLACKBERRY:
+            case _CONSTANTS.OS.WEB_TV:
+                osVersion = /(mac\sos\sx\s|iphone os\s|ipad;\scpu\sos\s|windows nt\s|android\s|blackberry\s|webtv\/)((\d|\.|_)*)/.exec(userAgent)[2].replace(/_/g, ".");
+                break;
+            default:
+                osVersion = null;
+        }
+
+        try {
+
+        } catch (error) {
+            console.warn("Trouble getting the os version, check the code or report a bug.");
+            osVersion = null;
+        }
     }
 
     var _CONSTANTS = {
@@ -40,7 +61,6 @@ Neosavvy.Core.Utils.BrowserUtils = (function () {
             WINDOWS: "win",
             LINUX: "linux",
             FREE_BSD: "freebsd",
-            GENERIC_MOBILE: "mobile",
             IPHONE: "iphone",
             IPOD: "ipod",
             IPAD: "ipad",
--- src/test/resources/library/utils/browser-spec.js
+++ src/test/resources/library/utils/browser-spec.js
@@ -210,16 +210,6 @@ describe("Neosavvy.Core.Utils.BrowserUtils", function () {
             });
         });
 
-        describe("GENERIC_MOBILE", function () {
-            it("Should match the platform when provided in the userAgent", function () {
-
-            });
-
-            it("Should work well with the is... call", function () {
-
-            });
-        });
-
         describe("IPHONE", function () {
             beforeEach(function () {
                 Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
@@ -412,6 +402,126 @@ describe("Neosavvy.Core.Utils.BrowserUtils", function () {
     });
 
     describe("osVersion ", function () {
+        describe("OSX", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");
+            });
 
+            it("Should return a decimalized version of the version for the os", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("10.8.5");
+            });
+        });
+
+        describe("WINDOWS", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (Windows NT 6.3; Win64; x64; Trident/7.0; rv:11.0) like Gecko");
+            });
+
+            it("Should provide the nt version of the windows os", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("6.3");
+            });
+        });
+
+        describe("LINUX", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("﻿Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:23.0) Gecko/20100101 Firefox/23.0");
+            });
+
+            it("Should have a null osVersion with linux", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toBeNull();
+            });
+        });
+
+        describe("FREE_BSD", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (X11; U; FreeBSD amd64; en-US; rv:1.8.0.8) Gecko/20061116 Firefox/1.5.0.8");
+            });
+
+            it("Should have a null osVersion with free bsd", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toBeNull();
+            });
+        });
+
+        describe("IPHONE", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
+            });
+
+            it("Should return the decimalized version of the iphone version", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("5.0");
+            });
+        });
+
+        describe("IPOD", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3A100a Safari/419.3");
+            });
+
+            it("Should return the decimalized version for the ipod in question", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toBeNull();
+            });
+        });
+
+        describe("IPAD", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
+            });
+
+            it("Should return a decimalized version for the iPad in question", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("5.0");
+            });
+        });
+
+        describe("ANDROID", function () {
+            describe("Phone", function () {
+                beforeEach(function () {
+                    Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (Linux; U; Android 2.3.4; fr-fr; HTC Desire Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
+                });
+
+                it("Should return the decimalized version 2.3.4 of the android platform", function () {
+                    expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("2.3.4");
+                });
+            });
+
+            describe("Tablet", function () {
+                beforeEach(function () {
+                    Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
+                });
+
+                it("Should return the decimalized version 2.3.4 of the android platform", function () {
+                    expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("4.0.4");
+                });
+            });
+        });
+
+        describe("BLACKBERRY", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.1.0.346 Mobile Safari/534.11+");
+            });
+
+            it("Should return the 9900 for the Blackberry version", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("9900");
+            });
+        });
+
+        describe("J2ME", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54");
+            });
+
+            it("Should return null for j2me", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toBeNull();
+            });
+        });
+
+        describe("WEB_TV", function () {
+            beforeEach(function () {
+                Neosavvy.Core.Utils.BrowserUtils.reload("Mozilla/3.0 WebTV/1.2 (compatible; MSIE 2.0)");
+            });
+
+            it("Should return the decimalized 1.2 for the web tv", function () {
+                expect(Neosavvy.Core.Utils.BrowserUtils.osVersion()).toEqual("1.2");
+            });
+        });
     });
 });
